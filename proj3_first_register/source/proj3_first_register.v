2b5a5be6f062cbec66e847cf671d49196c4057a5
Seamus B <brok.sea@gmail.com>
2026-03-01 21:58:55 -0800

    the toggle is not working yet. I'm sure it's some nonblocking assignment logic messed up in here. If this was a mastery-learning system I definitely would have smaller practice assignments on this functionality!

--- proj3_first_register/source/proj3_first_register.v
+++ proj3_first_register/source/proj3_first_register.v
@@ -1,14 +1,49 @@
-// Merrick, R. (2024). The Flip-Flop (aka Register).
-// Getting Started with FPGAs.
-// https://nandland.com/project-3-the-flip-flop-aka-register/
+/* 
+ * Merrick, R. (2024). The Flip-Flop (aka Register).
+ * Getting Started with FPGAs.
+ * https://nandland.com/project-3-the-flip-flop-aka-register/
+ */
 
 module proj3_first_register(
     input clk,
     input btn0,
-    output LED0);
+    output LED0,
+    output LED1);
+
+    reg r_LED0 = 1'b0;
+    reg r_btn0 = 1'b0;
+    reg r_sync_btn0 = 1'b0;
+    reg r_prev_btn0 = 1'b0;
+    reg [19:0] debounce_cnt = 0;
+    reg db_btn0 = 1'b0;
 
     always @(posedge clk) begin
-        assign LED0 = btn0;
+        // synchronize button by double-flopping
+        r_sync_btn0 <= btn0;
+        r_btn0 <= r_sync_btn0;
+
+        // debounce with timer
+        if( r_btn0 == db_btn0 ) begin
+            debounce_cnt <= 0;
+        end
+        else if( debounce_cnt == 20'hfffff ) begin
+            db_btn0 <= r_btn0;
+
+            // falling edge detection
+            if( !r_btn0 && r_prev_btn0 ) begin
+                r_LED0 <= !r_LED0;
+            end
+        end
+        else begin
+            debounce_cnt <= debounce_cnt + 1;
+        end
+
+        // update prev. state
+        r_prev_btn0 <= db_btn0;
+
     end
+
+    assign LED0 = r_LED0;
+    assign LED1 = btn0;
 
 endmodule
