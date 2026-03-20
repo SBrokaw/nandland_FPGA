6983b7928feb77937cb3fa00656b3e0255b9c1af
Seamus B <brok.sea@gmail.com>
2026-03-19 19:38:00 -0700

    Finished timer controlled LEDs! Needed to initialize the counter register.

--- proj6_LED_timers/source/proj6_LED_timers.v
+++ proj6_LED_timers/source/proj6_LED_timers.v
@@ -1,18 +1,22 @@
 /* 
- * Merrick, R. (2024). Seven Segment Display.
+ * Merrick, R. (2024). LED Timers.
  * Getting Started with FPGAs.
- * https://nandland.com/project-5-seven-segment-display/
+ * https://nandland.com/project-6-how-to-simulate-your-fpga-designs/
  *
- * Increment a Binary Counter to Drive One Digit of the 7-Segment 
- * Display Each Time a Switch is Released.
+ * Make the LEDs on the Go Board blink at different 
+ * rates: 1 Hz, 2 Hz, 5 Hz, and 10 Hz.
  */
 
- module proj5_seven_seg(
+ module proj6_LED_timers(
     input clk,
     input raw_btn0,
     input raw_btn1,
     output LED0,
-    output [6:0] segments);
+    output LED1,
+    output LED2,
+    output LED3,
+    output [6:0] segments0,
+    output [6:0] segments1);
 
     // debounce buttons
     wire btn0;
@@ -22,26 +26,40 @@
     debounce_btn db0( .clk(clk), .btn(raw_btn0), .db_btn(btn0));
     debounce_btn db1( .clk(clk), .btn(raw_btn1), .db_btn(btn1));
 
-    // form seven segment input
+    // initiate timers
+    wire ding_500ms;
+    wire ding_1000ms;
+    wire ding_2000ms;
+    timer #(.time_ms(200)) timer_500ms( .clk(clk), .timer_full(ding_500ms));
+    timer #(.time_ms(1000)) timer_1000ms( .clk(clk), .timer_full(ding_1000ms));
+    timer #(.time_ms(2000)) timer_2000ms( .clk(clk), .timer_full(ding_2000ms));
     reg r_LED0;
+    reg r_LED1;
+    reg r_LED2;
     reg [3:0] num = 4'h0;
+
+    /*
+     * MAIN clk loop
+     */
     always @(posedge clk) begin
         // rising edge detection
         if( !prev_btn0 & btn0 ) begin
             num <= num + 1;
             if( num > 4'hf ) num <= 4'h0;
         end
-
-        // falling edge detection
-        if( prev_btn0 & !btn0 ) begin
-            r_LED0 <= ~r_LED0;
-        end
-
         prev_btn0 <= btn0;
+
+        // timers control LEDs
+        if( ding_500ms ) r_LED2 <= ~r_LED2;
+        if( ding_1000ms ) r_LED0 <= ~r_LED0;
+        if( ding_2000ms ) r_LED1 <= ~r_LED1;
     end
 
-    // seven segment output
-    seven_seg ss0( .num(num), .segments(segments));
+    // outputs
+    seven_seg ss0( .num(num), .segments(segments0));
+    seven_seg ss1( .num(num % 2), .segments(segments1));
     assign LED0 = r_LED0;
+    assign LED1 = r_LED1;
+    assign LED2 = r_LED2;
 
  endmodule
--- proj6_LED_timers/source/timer.v
+++ proj6_LED_timers/source/timer.v
@@ -2,16 +2,16 @@
  * Timer asserts after __time_ms__ milliseconds.
  */
 
-module timer #(
+module timer
    #(parameter time_ms = 1000)
    (input clk,
     output timer_full);
 
     parameter CLK_FREQ = 25_000_000;
-    parameter TIMER_CNT = time_ms * CLK_FREQ / 1000;
+    parameter TIMER_CNT = CLK_FREQ / 1000 * time_ms;
 
-    reg [31:0] counter;
-    reg r_timer_full;
+    reg [31:0] counter = 0;
+    reg r_timer_full = 0;
 
     always @( posedge clk ) begin
         if( counter >= TIMER_CNT ) begin
